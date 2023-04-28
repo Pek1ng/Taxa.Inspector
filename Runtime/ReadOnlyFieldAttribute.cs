34a195bab4d11118648b76c1cbb2d769a0b22b09
Pek1ng <57554399+Pek1ng@users.noreply.github.com>
2023-04-29 00:58:22 +0800

    第一版😁

---
+++ Runtime/ReadOnlyFieldAttribute.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// Ö»¶Á×Ö¶Î
+/// </summary>
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+public class ReadOnlyFieldAttribute : PropertyAttribute
+{
+
+}
