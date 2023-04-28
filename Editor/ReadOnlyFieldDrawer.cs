13193c523b9c9f39d9e20e3f4cc76042d7ef7868
Pek1ng <57554399+Pek1ng@users.noreply.github.com>
2023-04-29 01:16:28 +0800

    改造

--- Editor/ReadOnlyFieldDrawer.cs
+++ Editor/ReadOnlyFieldDrawer.cs
@@ -1,6 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 
+[CustomPropertyDrawer(typeof(ReadOnlyFieldAttribute))]
 public class ReadOnlyFieldDrawer : PropertyDrawer
 {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
